d25e30b21dce31e4ef9cd7517636b3eacf5a0e49
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-01-15 16:17:54 -0500

    add zod

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,7 +1,29 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, Context } from "telegraf";
+import { logger } from "./logger";
+import { z } from "zod";
+
+const messageSchema = z.object({
+  message_id: z.number(),
+  from: z.object({
+    id: z.number(),
+    is_bot: z.boolean(),
+    first_name: z.string(),
+    last_name: z.string(),
+    username: z.string(),
+    language_code: z.string(),
+  }),
+  chat: z.object({
+    id: z.number(),
+    first_name: z.string(),
+    last_name: z.string(),
+    username: z.string(),
+  }),
+  date: z.number(),
+  text: z.string(),
+});
 
 const telegram_bot_token = process.env.TELEGRAM_API_KEY;
-if (telegram_bot_token === undefined) {
+if (!telegram_bot_token) {
   throw new Error("missing TELEGRAM_API_KEY");
 }
 
@@ -10,3 +32,10 @@ export const bot = new Telegraf(telegram_bot_token, {
 });
 
 bot.start((context) => context.reply("Hello"));
+
+bot.on("text", (context: Context) => {
+  const message = messageSchema.parse(context.message);
+  logger.info(`message: ${message.text}`);
+
+  context.reply(`Hello ${message.from.username}`);
+});
