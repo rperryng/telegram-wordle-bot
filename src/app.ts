aa1948b89d8872a9ef31a9a3c4048c424e664e0f
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-01-15 01:10:56 -0500

    refactor notfound to its own middleware

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,11 @@
 import Koa, {Context, Next} from 'koa';
 import serverless from 'serverless-http';
 import { logger } from './logger';
+import { notFound } from './middlewares/not-found';
 
 logger.info('hello world');
 const app = new Koa();
 
-// 404
-app.use(async (context: Context) => {
-  context.status = 404;
-
-  switch (context.accepts('html', 'json')) {
-    case 'html':
-      context.type = 'html';
-      context.body = '<p>Page Not Found</p>';
-      break;
-    case 'json':
-      context.type = 'json'
-      context.body = {
-        message: 'Page Not Found'
-      };
-      break;
-    default:
-      context.type = 'text';
-      context.body = 'Page Not Found';
-  }
-});
+app.use(notFound);
 
 export const handler = serverless(app);
