d3017d820bf00c21058ea8c5dc64c84d3cef5c7c
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-01-15 14:13:47 -0500

    up script builds tsc; log request bodies

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import Koa, { Context, Next } from "koa";
 import koaLogger from "koa-logger";
+import koaBodyParser from "koa-bodyparser";
 import serverless from "serverless-http";
 import { logger } from "./logger";
 import { notFound } from "./middlewares/not-found";
 
-logger.info("hello world");
-
 const app = new Koa();
 
 app.use(koaLogger((str, args) => logger.info(str, args)));
+app.use(koaBodyParser());
+app.use(async function (context, _next) {
+  logger.info(
+    `got request body: \n${JSON.stringify(context.request.body, null, 2)}`
+  );
+});
 app.use(notFound);
 
 export const handler = serverless(app);
