cb975a910bf2c8ecbc085d89797ddf153eb57eaf
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-01-15 11:19:39 -0500

    use ca-central-1; add request logging

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import Koa, { Context, Next } from "koa";
+import koaLogger from "koa-logger";
 import serverless from "serverless-http";
 import { logger } from "./logger";
 import { notFound } from "./middlewares/not-found";
@@ -6,6 +7,8 @@ import { notFound } from "./middlewares/not-found";
 logger.info("hello world");
 
 const app = new Koa();
+
+app.use(koaLogger((str, args) => logger.info(str, args)));
 app.use(notFound);
 
 export const handler = serverless(app);
