79675bbd1a7ab3177bf96b27b0aaec5fb834bd80
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-01-15 21:54:48 -0500

    parse response and respond

--- src/telegram/bot.ts
+++ src/telegram/bot.ts
@@ -1,21 +1,54 @@
 import { Telegraf, Context } from 'telegraf';
 import { logger } from '../logger';
 import { messageSchema } from './types';
+import { z } from 'zod';
+
+const WORDLE_SHARE_PATTERN =
+  /Wordle\s(?<wordleNumber>\d+)\s(?<numGuesses>\d)\/6\s+(?<guesses>(?:[\u{1F7E9}\u{2B1B}\u{1F7E8}]{5}\s?)*)/u;
+/Wordle\s(?<wordleNumber>\d+)\s(?<numGuesses>\d)\/6\s+/u;
+
+const submissionSchema = z.object({
+  wordleNumber: z.number(),
+  numGuesses: z.number(),
+  guesses: z.optional(z.string()),
+});
 
 const telegram_bot_token = process.env.TELEGRAM_API_KEY;
 if (!telegram_bot_token) {
   throw new Error('missing TELEGRAM_API_KEY');
 }
 
-export const bot = new Telegraf(telegram_bot_token, {
-  telegram: { webhookReply: true },
-});
+export const bot = new Telegraf(telegram_bot_token);
 
 bot.start((context) => context.reply('Hello'));
 
 bot.on('text', (context: Context) => {
   const message = messageSchema.parse(context.message);
-  logger.info(`message: ${message.text}`);
+  logger.info(`[message]: ${message.text}`);
 
-  context.reply(`Hello ${message.from.username}`);
+  const match = message.text.match(WORDLE_SHARE_PATTERN);
+
+  logger.info(`match: ${match}`);
+
+  if (!match || !match.groups) {
+    logger.info('board not valid');
+    context.reply('this board is not valid');
+    return;
+  }
+
+  logger.info(`submission parsed: ${JSON.stringify(match.groups, null, 2)}`);
+
+  const submission = submissionSchema.parse({
+    wordleNumber: parseInt(match.groups.wordleNumber),
+    numGuesses: parseInt(match.groups.numGuesses),
+    guesses: match.groups.guesses,
+  });
+
+  context.reply(`Thank you for your submission ${message.from.username}
+Worldle #${submission.wordleNumber}
+Number of guesses: ${submission.numGuesses}
+guesses:
+${submission.guesses}`);
+
+  logger.info('succeeded');
 });
