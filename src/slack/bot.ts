7a3a02f735e653d08c4fc545feec1e81b64bbc12
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-01-29 12:30:46 -0500

    functional slack entry point

---
+++ src/slack/bot.ts
@@ -0,0 +1,17 @@
+import { App, AwsLambdaReceiver } from '@slack/bolt';
+import { env } from '../env';
+
+const config = {
+  token: env('SLACK_BOT_TOKEN'),
+  signingSecret: env('SLACK_SIGNING_SECRET'),
+};
+
+export const receiver = new AwsLambdaReceiver({
+  signingSecret: config.signingSecret,
+});
+
+export const bot = new App({
+  token: config.token,
+  receiver,
+  processBeforeResponse: true,
+});
